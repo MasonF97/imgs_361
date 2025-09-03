98c638e6b521fa51d2e1dad517e458a47234da8f
MasonF97 <mflaat@outlook.com>
2025-09-03 16:45:40 -0400

    IGS working

--- imgs/ipcv/quantize/Quantize.cpp
+++ imgs/ipcv/quantize/Quantize.cpp
@@ -25,6 +25,30 @@ static int _is_power_of_two(const int number) {
   return log_2_double == log_2_int;
 }
 
+
+static void _transform_all_digital_counts_uniform(const cv::Mat& src, cv::Mat& dst, std::function<uint8_t(uint8_t)> transform) {
+  for (int r = 0; r < src.rows; r++) {
+    for (int c = 0; c < src.cols; c++) {
+      uint8_t src_value = src.at<cv::Vec3b>(r, c)[0];
+      uint8_t quantized_value = transform(src_value);
+      dst.at<cv::Vec3b>(r, c) = cv::Vec3b(quantized_value, quantized_value, quantized_value);
+    }
+  }
+}
+
+// static void _transform_all_digital_counts_igs(const cv::Mat& src, cv::Mat& dst, std::function<int(int, uint8_t)> transform) {
+//   uint8_t remainder = 0;
+//   for (int r = 0; r < src.rows; r++) {
+//     for (int c = 0; c < src.cols; c++) {
+//       int src_value = src.at<cv::Vec3b>(r, c)[0];
+//       if 
+//       uint8_t quantized_value = transform(src_value, remainder);
+
+//       dst.at<cv::Vec3b>(r, c) = cv::Vec3b(quantized_value, quantized_value, quantized_value);
+//     }
+//   }
+// }
+
 /** Perform uniform grey-level quantization on a color image
  *
  *  \param[in] src                 source cv::Mat of CV_8UC3
@@ -35,28 +59,23 @@ static int _is_power_of_two(const int number) {
 void Uniform(const cv::Mat& src, const int quantization_levels, cv::Mat& dst) {  
   // Check whether the quantization level number is a power of two
   // Must be a float to avoid errors with odd quantization levels
+  std::function<uint8_t(uint8_t)> transform;
   float quantization_denominator = (256.0 / quantization_levels);
   if (_is_power_of_two(quantization_levels)){
     // Use bit shifting
     uint8_t bit_shift = static_cast<int>(log2(quantization_denominator));
-    for (int r = 0; r < src.rows; r++) {
-      for (int c = 0; c < src.cols; c++) {
-        uint8_t src_value = src.at<cv::Vec3b>(r, c)[0];
-        uint8_t quantized_value = src_value >> bit_shift;
-        dst.at<cv::Vec3b>(r, c) = cv::Vec3b(quantized_value, quantized_value, quantized_value);
-      }
-    }
+    transform = [bit_shift](int digital_count) {
+      return digital_count >> bit_shift;
+    };
   }
   else{
     // Use division
-    for (int r = 0; r < src.rows; r++) {
-      for (int c = 0; c < src.cols; c++) {
-        uint8_t src_value = src.at<cv::Vec3b>(r, c)[0];
-        uint8_t quantized_value = src_value / quantization_denominator;
-        dst.at<cv::Vec3b>(r, c) = cv::Vec3b(quantized_value, quantized_value, quantized_value);
-      }
-    }
+    transform = [quantization_denominator](int digital_count) {
+      return digital_count / quantization_denominator;
+    };
   }
+
+  _transform_all_digital_counts_uniform(src, dst, transform);
 }
 
 /** Perform improved grey scale quantization on a color image
@@ -82,7 +101,43 @@ void Igs(const cv::Mat& src, const int quantization_levels, cv::Mat& dst) {
 
 
   if (_is_power_of_two(quantization_levels)){
-    quantization_denominator = 1;
+    uint8_t bit_shift = static_cast<int>(log2(quantization_denominator));
+    uint8_t remainder = 0;
+    for (int r = 0; r < src.rows; r++) {
+      for (int c = 0; c < src.cols; c++) {
+        uint8_t src_value = src.at<cv::Vec3b>(r, c)[0];
+        // If adding the remainder would cause an overflow, make the remainder 0
+        if ((remainder + src_value) > 255){
+          remainder = 0;
+        }
+        // uint8_t quantized_value = transform(src_value, remainder);
+        uint8_t quantized_value = (src_value + remainder) >> bit_shift;
+        // Use the bitwise and operator to get the 4 least significant bits, which is the remainder
+        remainder = src_value & 0b00001111;
+        
+        dst.at<cv::Vec3b>(r, c) = cv::Vec3b(quantized_value, quantized_value, quantized_value);
+      }
+    }
+    
+  }
+  else{
+    // Use division
+    float remainder = 0;
+    for (int r = 0; r < src.rows; r++) {
+      for (int c = 0; c < src.cols; c++) {
+        uint8_t src_value = src.at<cv::Vec3b>(r, c)[0];
+        // If adding the remainder would cause an overflow, make the remainder 0
+        if ((remainder + src_value) > 255){
+          remainder = 0;
+        }
+        // uint8_t quantized_value = transform(src_value, remainder);
+        uint8_t quantized_value = (src_value + remainder) / quantization_denominator;
+        // Use fmod instead of modulo so it doesn't have to be an int
+        remainder = fmod(src_value, quantization_denominator);
+        
+        dst.at<cv::Vec3b>(r, c) = cv::Vec3b(quantized_value, quantized_value, quantized_value);
+      }
+    }
   }
 
 }

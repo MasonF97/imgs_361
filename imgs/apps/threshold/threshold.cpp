7fce6a1fabb536e8f4d8da79a114133abe9fcfb7
MasonF97 <mflaat@outlook.com>
2025-09-17 18:35:09 -0400

    plots now working

--- imgs/apps/threshold/threshold.cpp
+++ imgs/apps/threshold/threshold.cpp
@@ -16,27 +16,6 @@ using namespace std;
 
 namespace po = boost::program_options;
 
-// void plot_channel_from_mat_row(const cv::Mat& pdf_mat, int row_index,
-//                                const std::string& title,
-//                                const std::string& color,
-//                                double threshold) {
-//   std::vector<std::pair<int, float>> pdf_data;
-//   for (int i = 0; i < pdf_mat.cols; ++i) {
-//       pdf_data.emplace_back(i, pdf_mat.at<float>(row_index, i));
-//   }
-
-//   Gnuplot gp;
-//   gp << "set title '" << title << " Channel PDF'\n";
-//   gp << "set xlabel 'Pixel Intensity'\n";
-//   gp << "set ylabel 'Probability Density'\n";
-//   gp << "set style line 1 lc rgb '" << color << "' lw 2\n";
-//   gp << "set style line 2 lc rgb 'black' dt 2 lw 1\n";
-//   gp << "plot '-' with lines ls 1 title '" << title << " PDF', \\\n";
-//   gp << threshold << " with lines ls 2 title 'Otsu Threshold (" << threshold << ")'\n";
-//   gp.send1d(pdf_data);
-//   gp << "pause -1\n";
-// }
-
 int main(int argc, char* argv[]) {
   bool verbose = false;
   string src_filename = "";
@@ -99,36 +78,37 @@ int main(int argc, char* argv[]) {
     cout << threshold << endl;
   }
 
+  // If verbose create plots that show the pdf with a vertical line at the threshold value
+  // Done in a similar way to plot_histogram.cpp
   if (verbose) {
-
-    cv::Mat_<int> dc;
-    dc = cv::Mat_<int>::zeros(3, 256);
-    for (int i = 0; i < 256; i++) {
-      dc.at<int>(0, i) = i;
-      dc.at<int>(1, i) = i;
-      dc.at<int>(2, i) = i;
-    }
+    // Create the pdf
     cv::Mat_<int> h;
     ipcv::Histogram(src, h);
     cv::Mat_<double> pdf;
     ipcv::HistogramToPdf(h, pdf);
-    // cv::Mat single_color_pdf = pdf.row(i).clone();
 
-    // plot_channel_from_mat_row(pdf, 2, "Red", "red", threshold[0]);
-    // plot_channel_from_mat_row(pdf, 1, "Green", "green", threshold[1]);
-    // plot_channel_from_mat_row(pdf, 0, "Blue", "blue", threshold[2]);
-
-    plot::plot2d::Params params;
-    params.set_x_label("Digital Count");
-    params.set_x_min(0);
-    params.set_x_max(255);
-    // params.xvline(threshold[0]);
-    // params.set_destination_filename(dst_filename);
-
-    double max_value;
-    cv::minMaxLoc(pdf, NULL, &max_value, NULL, NULL);
-    params.set_y_label("Probability Density");
-    plot::plot2d::Plot2d(dc, pdf, params);
+    // Create a vector to represent the x axis
+    std::vector<double> x_vector(256);
+    for (int i = 0; i < 256; i++) {
+      x_vector[i] = i;
+    }
+  
+    for(int x = 0; x < 3; x++){
+      // create a vector to represent the y axis 
+      std::vector<double> y_vector(256);
+      for (int i = 0; i < 256; ++i) {
+        y_vector[i] = pdf.at<double>(x, i);
+      }
+      plot::plot2d::Params params;
+      params.set_x_label("Digital Count");
+      params.set_x_min(0);
+      params.set_x_max(255);
+      // set the threshold value
+      params.set_xvline(static_cast<double>(threshold[x]));
+      params.set_y_label("Probability Density");
+      // Create the plot
+      plot::plot2d::Plot2d(x_vector, y_vector, params);
+    }
   }
 
   cv::Mat lut;

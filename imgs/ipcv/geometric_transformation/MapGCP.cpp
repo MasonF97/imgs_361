6ee9a2bfa3d2cbffced64a0133c1ee8ce1a6c516
MasonF97 <mflaat@outlook.com>
2025-09-30 12:11:39 -0400

    polynomial pairs generated correctly

--- imgs/ipcv/geometric_transformation/MapGCP.cpp
+++ imgs/ipcv/geometric_transformation/MapGCP.cpp
@@ -9,6 +9,8 @@
 #include "MapGCP.h"
 
 #include <iostream>
+#include <vector>
+#include <utility>
 
 #include <Eigen/Dense>
 #include <opencv2/core.hpp>
@@ -16,6 +18,18 @@
 using namespace std;
 
 namespace ipcv {
+
+vector<std::pair<int, int>> get_polynomial_pairs(int order){
+  vector<std::pair<int, int>> polynomial_pairs;
+  for (int y = 0; y<= order; y++){
+    for (int x = 0; x<= order; x++){
+      if (x+y <= order){
+        polynomial_pairs.push_back({x,y});
+      }
+    }
+  }
+  return polynomial_pairs;
+}
 
 /** Find the source coordinates (map1, map2) for a ground control point
  *  derived mapping polynomial transformation
@@ -53,8 +67,43 @@ bool MapGCP(const cv::Mat src, const cv::Mat map,
             const vector<cv::Point> src_points,
             const vector<cv::Point> map_points, const int order,
             cv::Mat& map1, cv::Mat& map2) {
+  // Use a psuedo inverse for x
+  // a = (Xt X)^-1 * Xt Y
+  // X transpose * X gives a square matrix
+  // Then get the inverse
+  // Then get multiply it by the transpose again
+  // Then multiply by the Y
+  /*
+  The sums of the exponents are not greater than 2, so it's a second order polynomial
+  x' = a0 x0 y0 + a1 x1 y0 + a2 x0 y1 + a3 x1 y1 + a4 x2 y0 + a5 x0 y2
+  Can make this using 2 loops
+  x0 y0
+  x0 y1 x1 y0
+  x0 y2 x1  y1 x2 y0
+  (Reverse the order)
 
-  // Insert your code here
+  Model matrix = 6 terms in the equation, so 6 terms in the model equation
+  X = 1 x1 y1 x1y1 x2 y2
+  This is multiple linear least squares regression
+  Observation matrix Y': Two matrices x' and y'
+
+  Solve for A's and B's
+  a = (Xt X)^-2 * Xt Yx'
+  b = (Xt X)^-2 * Xt Yy'
+  */
+
+  // Construct model matrix
+
+  // Get polynomials
+  vector<std::pair<int, int>> polynomial_pairs = get_polynomial_pairs(order);
+
+  for (std::pair<int, int> pair : polynomial_pairs){
+    cout << pair.first << " " << pair.second << endl;
+  }
+
+  // Construct observation matrices
+
+
 
   return true;
 }

d245552ea9a4d7957d5f527425ce0e3e1122d08b
MasonF97 <mflaat@outlook.com>
2025-10-16 18:38:07 -0400

    comments

--- imgs/ipcv/geometric_transformation/MapQ2Q.cpp
+++ imgs/ipcv/geometric_transformation/MapQ2Q.cpp
@@ -38,7 +38,7 @@ bool MapQ2Q(const cv::Mat src, const cv::Mat tgt,
             const vector<cv::Point> tgt_vertices, cv::Mat& map1,
             cv::Mat& map2) {
 
-  
+  // create the model matrix
   Eigen::MatrixXd model_matrix(8,8);
   model_matrix << tgt_vertices[0].x, tgt_vertices[0].y, 1, 0, 0, 0, tgt_vertices[0].x*-1*src_vertices[0].x, tgt_vertices[0].y*-1*src_vertices[0].x,
     tgt_vertices[1].x, tgt_vertices[1].y, 1, 0, 0, 0, tgt_vertices[1].x*-1*src_vertices[1].x, tgt_vertices[1].y*-1*src_vertices[1].x,
@@ -49,8 +49,9 @@ bool MapQ2Q(const cv::Mat src, const cv::Mat tgt,
     0, 0, 0, tgt_vertices[2].x, tgt_vertices[2].y, 1, tgt_vertices[2].x*-1*src_vertices[2].y, tgt_vertices[2].y*-1*src_vertices[2].y,
     0, 0, 0, tgt_vertices[3].x, tgt_vertices[3].y, 1, tgt_vertices[3].x*-1*src_vertices[3].y, tgt_vertices[3].y*-1*src_vertices[3].y;
 
-  Eigen::MatrixXd other_matrix(8,1);
-  other_matrix << src_vertices[0].x,
+  // make the source coordinate vector
+  Eigen::MatrixXd src_coords_matrix(8,1);
+  src_coords_matrix << src_vertices[0].x,
     src_vertices[1].x,
     src_vertices[2].x,
     src_vertices[3].x,
@@ -59,7 +60,9 @@ bool MapQ2Q(const cv::Mat src, const cv::Mat tgt,
     src_vertices[2].y,
     src_vertices[3].y;
 
-  Eigen::MatrixXd coefficents_matrix = model_matrix.inverse() * other_matrix;
+  // Multiply the matrices to get the coefficents matrix
+  Eigen::MatrixXd coefficents_matrix = model_matrix.inverse() * src_coords_matrix;
+  // Use the coeffs to create the map to image matrix
   Eigen::Matrix3d map_to_image_matrix;
   map_to_image_matrix << coefficents_matrix(0,0), coefficents_matrix(1,0), coefficents_matrix(2,0),
     coefficents_matrix(3,0), coefficents_matrix(4,0), coefficents_matrix(5,0),
@@ -79,6 +82,7 @@ bool MapQ2Q(const cv::Mat src, const cv::Mat tgt,
         v,
         1;
 
+      // Get the mapping coords by multiplying the map to image matrix and the target coords matrix
       Eigen::MatrixXd source_matrix = map_to_image_matrix * target_matrix;
       double w = source_matrix(2, 0);
       map1.at<float>(v, u) = source_matrix(0,0) / w;
